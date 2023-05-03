63b6a3ba8d373357c3c38fb7389b90bc37c79260
Edivandro Colaço <edileal1@gmail.com>
2023-05-03 16:31:27 -0300

    Try catch finalizado

--- Pomoday.Api/Controllers/UsuarioController.cs
+++ Pomoday.Api/Controllers/UsuarioController.cs
@@ -28,61 +28,99 @@ namespace Pomoday.Api.Controllers
         [ProducesResponseType(201)]
         public async Task<ActionResult<UsuarioResponse>> Post([FromBody] UsuarioRequest usuario)
         {
-            var result = await _usuarioService.CriarAsync(usuario);
-            return Created(nameof(Post), result);
+            try
+            {
+                var result = await _usuarioService.CriarAsync(usuario);
+                return Created(nameof(Post), result);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
         /// <summary>
         /// Realiza uma busca de usuário por Id.
         /// </summary>
         /// <returns>Usuário</returns>
         /// <response code="200">Retorna usuário</response>
+        /// <response code="404">Se o usuário não foi encontrado</response>
         [HttpGet("{id}")]
         [SwaggerOperation(Summary = "Busca um usuário por Id", Description = "Retorna o usuário se ele for encontrado. Caso contrário, retorna exception.")]
         [ProducesResponseType(200)]
         public async Task<ActionResult<UsuarioResponse>> GetById(Guid id)
         {
-            var result = await _usuarioService.ObterPorIdAsync(id);
-            return Ok(result);
+            try
+            {
+                var result = await _usuarioService.ObterPorIdAsync(id);
+                return Ok(result);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
         /// <summary>
         /// Realiza busca de todos os usuários.
         /// </summary>
         /// <returns>Usuário</returns>
         /// <response code="200">Retorna usuários</response>
+        /// <response code="404">Se os usuários não foram encontrados</response>
         [HttpGet]
-        [SwaggerOperation(Summary = "Busca todos os usuários ativos.", Description = "Retorna todos os usuários Ativos.")]
+        [SwaggerOperation(Summary = "Busca todos os usuários ativos.", Description = "Retorna todos os usuários ativos.")]
         [ProducesResponseType(200)]
         public async Task<ActionResult<IEnumerable<UsuarioResponse>>> Get()
         {
-            var result = await _usuarioService.ObterTodosAsync();
-            return Ok(result);
+            try
+            {
+                var result = await _usuarioService.ObterTodosAsync();
+                return Ok(result);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         /// <summary>
         /// Busca um usuário e realiza mudança de dados.
         /// </summary>   
         ///<returns>Usuário modificado</returns>
-        /// <response code="200">Se o objeto existe e foi alterado</response>
+        /// <response code="200">Se o usuário existe e foi alterado</response>
+        /// <response code="404">Se o usuário não foi encontrado</response>
         [HttpPut("{id}")]
         [ProducesResponseType(200)]
         [SwaggerOperation(Summary = "Busca usuário para mudança de dados.", Description = "Retorna o usuário modificado.")]
         public async Task<ActionResult<UsuarioResponse>> Put([FromBody] UsuarioAlteracaoRequest request, [FromRoute] Guid id)
         {
-            var result = await _usuarioService.PutUsuario(request, id);
-            return Ok(result);
+            try
+            {
+                var result = await _usuarioService.PutUsuario(request, id);
+                return Ok(result);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         /// <summary>
         /// Deleta usuário.
         /// </summary>            
-        /// <response code="200">Se o objeto existe</response>
-        /// <response code="404">Se o objeto não existe</response>
+        /// <response code="200">Se o usuário existe</response>
+        /// <response code="404">Se o usuário não existe</response>
         [HttpDelete("{id}")]
         [ProducesResponseType(204)]
         public async Task<ActionResult> Delete([FromRoute] Guid id)
         {
-            await _usuarioService.DeletarAsync(id);
-            return NoContent();
+            try
+            {
+                await _usuarioService.DeletarAsync(id);
+                return NoContent();
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
     }
 }
