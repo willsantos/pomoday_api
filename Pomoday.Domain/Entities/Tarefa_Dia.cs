2f83edf9eb3f09c3f214efe6c65f5692dda61536
Edivandro Colaço <edileal1@gmail.com>
2023-05-09 15:45:03 -0300

    Tarefa_Dia entity

---
+++ Pomoday.Domain/Entities/Tarefa_Dia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomoday.Domain.Entities
+{
+    public class Tarefa_Dia
+    {
+        public Guid Id { get; set; }
+        public virtual Dia Dia{ get; set; }
+        public virtual Projeto Projeto { get; set; }
+
+        #region Foreign key
+        public Guid DiaId { get; set; }
+        public Guid ProjetoId { get; set; }
+        #endregion
+    }
+}
