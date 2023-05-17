46f0cb1b4dce596710822b3a5a98ca5ba1122481
Edivandro Colaço <edileal1@gmail.com>
2023-05-17 15:35:39 -0300

    Nova migration

---
+++ Pomoday.Repository/Migrations/20230517183307_CampoNomeTarefa.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pomoday.Repository.Migrations
+{
+    public partial class CampoNomeTarefa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Prazo",
+                table: "Tarefas",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Agendada",
+                table: "Tarefas",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "Tarefas",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Progresso",
+                table: "Projetos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "Tarefas");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Prazo",
+                table: "Tarefas",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Agendada",
+                table: "Tarefas",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Progresso",
+                table: "Projetos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
